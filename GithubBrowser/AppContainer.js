d4504d7fc899cf916f87396a5330c948b52ccde3
Pratik Infanto <infanto.pratik@gmail.com>
2017-01-25 14:53:25 +0530

    asd

--- GithubBrowser/AppContainer.js
+++ GithubBrowser/AppContainer.js
@@ -22,10 +22,16 @@ export default class AppContainer extends Component {
         <TabBarIOS.Item
           title = "Feed"
           selected = {this.state.selectedTab == 'feed'}
-          icon = {require('./img/inbox.png')}
           onPress = { () => this.setState({ selectedTab: 'feed' }) }
         >
           <Text style = {styles.welcome}>Tab 1</Text>
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          title = "Search"
+          selected = {this.state.selectedTab == 'search'}
+          onPress = { () => this.setState({ selectedTab: 'search' }) }
+        >
+          <Text style = {styles.welcome}>Tab 2 BOOYAH</Text>
         </TabBarIOS.Item>
       </TabBarIOS>
     );
@@ -37,11 +43,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
+  }
 });
---
+++ GithubBrowser/feed.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+export default class Feed extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
